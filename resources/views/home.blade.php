fc69454cc199a7bc2dff800c9b331c8746763e26
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-07 14:39:02 +0700

    feat: menambah style untuk home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Page</title>
+    <style>
+        body {
+            font-family: Arial;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+            text-align: center;
+        }
+        .navbar {
+            background: #007BFF;
+            color: white;
+            text-align: center;
+        }
+        .navbar a {
+            color: white;
+            text-decoration: none;
+            padding: 10px ;
+            display: inline-block;
+        }
+        .navbar a:hover {
+            background: blue;
+            border-radius: 10px;
+        }
+        .selamatdatang {
+            margin-top: 20px;
+            padding: 20px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Home Page</h1>
-    <br>
-    <a href="{{ route('user.showUser', ['id' => 2341720102, 'name' => 'Muhammad Syafiq Aldiansyah']) }}"><h3>Profile Page</h3></a>
-    <a href="{{ route('category.showCategory') }}"><h3>Category Page</h3></a>
-    <a href=" {{ route('transaction.showTransaction') }}"><h3>History Transaction Page</h3></a>
+
+    <div class="navbar">
+        <a href="{{ route('user.showUser', ['id' => 2341720102, 'name' => 'Muhammad Syafiq Aldiansyah']) }}">Profile Page</a>
+        <a href="{{ route('category.showCategory') }}">Category Page</a>
+        <a href="{{ route('transaction.showTransaction') }}">History Transaction Page</a>
+    </div>
+
+    <div class="selamatdatang">
+        <h1>Welcome to Home Page</h1>
+    </div>
+
 </body>
 </html>
