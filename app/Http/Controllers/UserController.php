632af6dd293d3c60e1dbdad409de1232fb88b4d3
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-13 13:55:35 +0700

    22_Jobsheet3_Praktikum6-5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function index(){
+        $user = UserModel::all();               // Mengambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
+
     public function showUser($id = 22, $name = 'Syafiq'){
         return view('user', ['id' => $id, 'name' => $name]);
     }
