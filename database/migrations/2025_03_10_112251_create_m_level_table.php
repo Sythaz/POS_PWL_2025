1eb14530d68f2d0ffbc9a1b44f355834b6a418ef
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-13 12:25:04 +0700

    22_Jobsheet3_Praktikum2-2(Hasil)

--- database/migrations/2025_03_10_112251_create_m_level_table.php
+++ database/migrations/2025_03_10_112251_create_m_level_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('m_level', function (Blueprint $table) {
             $table->id('level_id');
-            $table->string('level_kode', 10)->unique();
-            $table->string('level_nama', 100);
+            $table->string('kategori_kode', 10)->unique();
+            $table->string('kategori_nama', 100);
             $table->timestamps();
         });
     }
---
+++ database/migrations/2025_03_13_094101_create_t_stok_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_stok', function (Blueprint $table) {
+            $table->id('stok_id');
+            
+            // unsignedBigInteger = bilangan bulat yang tidak negatif
+            // Menggunakan unsigned karena kita memisahkan kolom dan foreign
+            // Bisa saja kolom langsung, tapi menggunakan foreignId sehingga tidak memisahkan kolom dan foreign
+            $table->unsignedBigInteger('supplier_id');
+            $table->unsignedBigInteger('barang_id');
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('stok_tanggal');
+            $table->integer('stok_jumlah');
+            $table->timestamps();
+
+            // Foreign Key
+            $table->foreign('supplier_id')->references('supplier_id')->on('m_supplier');
+            $table->foreign('barang_id')->references('barang_id')->on('m_barang');
+            $table->foreign('user_id')->references('user_id')->on('m_user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_stok');
+    }
+};
