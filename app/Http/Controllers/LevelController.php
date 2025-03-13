d002ef717357925f8db790cba1639ed352f57821
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-13 13:36:07 +0700

    ganti nama kolum pada level

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -10,14 +10,14 @@ class LevelController extends Controller
     public function index()
     {
         // DB::insert(
-        //     'INSERT INTO m_level(kategori_kode, kategori_nama, created_at) VALUES (?, ?, ?)', ['CUS', 'Pelanggan', now()]
+        //     'INSERT INTO m_level(level_kode, level_nama, created_at) VALUES (?, ?, ?)', ['CUS', 'Pelanggan', now()]
         // );
         // return 'Insert data baru berhasil';
 
-        // $row = DB::update('UPDATE m_level SET kategori_nama = ? WHERE kategori_kode = ? ', ['Customer', 'CUS' ]);
+        // $row = DB::update('UPDATE m_level SET level_nama = ? WHERE level_kode = ? ', ['Customer', 'CUS' ]);
         // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row.' baris';
 
-        // $row = DB::delete('DELETE FROM m_level WHERE kategori_kode = ?', ['CUS']);
+        // $row = DB::delete('DELETE FROM m_level WHERE level_kode = ?', ['CUS']);
         // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row. ' baris';
 
         $data = DB::select('SELECT * FROM m_level');
