e52f03c3c6122e8639caa483467f1732e046bcdd
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-13 13:56:55 +0700

    22_Jobsheet3_Praktikum6-6

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,35 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Profile User Page</title>
-    <style>
-        body {
-            padding-left: 20px;
-            padding-right: 20px;
-        }
-        .container {
-            display: flex;
-            justify-content: space-between;
-        }
-        img {
-            width: 150px;
-            height: 150px;
-        }
-    </style>
-</head>
-<body>
-    <h1>User Page</h1>
-    <div class="container">
-        <div>
-            <p><strong>ID NIM:</strong> {{ $id }}</p>
-            <p><strong>Nama:</strong> {{ $name }}</p>
-        </div>
-        <div>
-            <img src="https://media.licdn.com/dms/image/v2/D5603AQGVk2YuWFWmmA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1692776958309?e=1746662400&v=beta&t=Q6tobOmcx7a4wQzQXQa1p2pi1JZkHpEUptM1E6WNrGk">
-        </div>
-    </div>
-</body>
-</html>
+<! DOCTYPE html>
+    <html>
+
+    <head>
+        <title>Data User</title>
+    </head>
+
+    <body>
+        <h1>Data User</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+            </tr>
+            @foreach ($data as $d)
+                <tr>
+                    <td>{{ $d->user_id }}</td>
+                    <td>{{ $d->username }}</td>
+                    <td>{{ $d->nama }}</td>
+                    <td>{{ $d->level_id }}</td>
+                </tr>
+            @endforeach
+        </table>
+    </body>
+
+    </html>
