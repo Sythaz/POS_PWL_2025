b8b76df30f5ba5c9fb7ddf5e1843a254fe70a014
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-14 19:21:16 +0700

    22 - Pertemuan 4 - Praktikum2.1-1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,22 +8,14 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index(){
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-
-        UserModel::create($data);               // Menambahkan data ke tabel m_user
-        // ::create, template kode untuk menambahkan data ke tabel yang terhubung dengan model
-
-        $user = UserModel::all();               // Mengambil semua data dari tabel m_user
+    public function index()
+    {
+        $user = UserModel::find(1);               // Mengambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 
-    public function showUser($id = 22, $name = 'Syafiq'){
+    public function showUser($id = 22, $name = 'Syafiq')
+    {
         return view('user', ['id' => $id, 'name' => $name]);
     }
 }
