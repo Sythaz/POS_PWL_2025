d5a96fd943f987a77356268231676d4fee6d0270
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-14 19:27:01 +0700

    22 - Pertemuan 4 - Praktikum2.1-2 (revisi)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -15,10 +15,10 @@
                 <th>ID Level Pengguna</th>
             </tr>
             <tr>
-                <td>{{ $d->user_id }}</td>
-                <td>{{ $d->username }}</td>
-                <td>{{ $d->nama }}</td>
-                <td>{{ $d->level_id }}</td>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
             </tr>
         </table>
     </body>
