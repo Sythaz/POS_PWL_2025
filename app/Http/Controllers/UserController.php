c411662ffddd08ae046eabc418289a0ea1a32d37
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-14 22:16:20 +0700

    22 - Pertemuan 4 - Praktikum2.5-2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,30 +11,21 @@ class UserController extends Controller
     public function index()
     {
         $user = UserModel::create([
-            'username' => 'manager55',
-            'nama' => 'Manager55',
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2,
         ]);
-        
-        $user->username = 'manager56';
+        $user->username = 'manager12';
 
-        $user->isDirty();                       // true
-        $user->isDirty('username');             // true
-        $user->isDirty('nama');                 // false
-        $user->isDirty(['nama', 'username']);   // true
-        
-        $user->isClean();                       // false
-        $user->isClean('username');             // false
-        $user->isClean('nama');                 // true
-        $user->isClean(['nama', 'username']);   // false
-        
         $user->save();
-        
-        $user->isDirty();                       // false
-        $user->isClean();                       // true
 
-        dd($user->isDirty());
+        $user->wasChanged();                            // true
+        $user->wasChanged('username');                  // true
+        $user->wasChanged(['username', 'level_id']);    // true, karena wasChanged([]) menggunakan OR
+        $user->wasChanged('nama');                      // false
+
+        dd($user->wasChanged(['nama', 'username']));    // true
     }
 
     public function showUser($id = 22, $name = 'Syafiq')
