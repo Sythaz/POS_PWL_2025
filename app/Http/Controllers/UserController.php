c0e5221108bdd88759f77e63f71239625df55159
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-14 20:07:07 +0700

    22 - Pertemuan 4 - Praktikum2.3-3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
         $user = UserModel::where('level_id', 2)->count(); // Menghitung jumlah user dengan level_id = 2
         // Mirip seperti break yang menghentikan kode dibawahnya, namun ini juga bisa digunakan untuk debugging
         // Seperti paduan print dan break
-        dd($user); 
+        // dd($user); 
 
         return view('user', ['data' => $user]);
     }
