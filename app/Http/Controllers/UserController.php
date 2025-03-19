464c764ad49a5287afc6126a622b0f3b35173e57
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-19 07:52:57 +0700

    22 - Pertemuan 5 - Praktikum3-11

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -62,6 +62,25 @@ class UserController extends Controller
         return view('user.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
+    // Menyimpan data user baru
+    public function store(Request $request)
+    {
+        $request->validate([
+            // username harus diisi, berupa string, minimal 3 karakter, dan bernilai unik di tabel m_user kolom username
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama' => 'required|string|max:100',         // nama harus diisi, berupa string, dan maksimal 100 karakter
+            'password' => 'required|min:5',       // password harus diisi dan minimal 5 karakter
+            'level_id' => 'required|integer',      // level_id harus diisi dan berupa angka
+        ]);
+
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => bcrypt($request->password), // password dienkripsi sebelum disimpan
+            'level_id' => $request->level_id
+        ]);
+        return redirect('/user')->with('success', 'Data user berhasil disimpan');
+    }
 
     public function showUser($id = 22, $name = 'Syafiq')
     {
