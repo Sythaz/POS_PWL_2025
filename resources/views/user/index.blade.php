1d8633475356d26d148de1962a2400dc29a3b10e
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-19 15:33:55 +0700

    22 - Pertemuan 5 - Praktikum3-23 (revisi)

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -12,20 +12,22 @@
             @if (session('success'))
                 <div class="alert alert-success">{{ session('success') }}</div>
             @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
             <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
                     <tr>
                         <th>ID</th>
                         <th>Username</th>
                         <th>Nama</th>
-                        <th>Level
-
-                            Pengguna</th>
+                        <th>Level Pengguna</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
             </table>
         </div>
+
     </div>
 @endsection
 
