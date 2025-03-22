a1b6c3eded4a9e857d7840817cd8bdbc802ed32f
Sythaz <syafiqaldiansyah@gmail.com>
2025-03-22 10:12:19 +0700

    22 - Pertemuan 6 - Praktikum2-3

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}', [UserController::class, 'show']);       // menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']);  // Menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']);     // menyimpan perubahan data user
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);        // Menampilkan halaman form edit user Ajax
+    Route::post('/{id}/update_ajax', [UserController::class, 'update_ajax']);   // Menyimpan perubahan data user Ajax
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
 });
 
